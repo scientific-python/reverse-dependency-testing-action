df26b1187e3cfcea3a6ae235d3d2013f018f9842
Martin Fleischmann <martin@martinfleischmann.net>
2023-05-23 12:54:33 -0700

    cd

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,8 @@ set -e
 # enable trace mode (print what it does)
 set -x
 
+cd ..
+
 # micromamba repoquery whoneeds $INPUT_PACKAGE_NAME -c conda-forge > whoneeds.txt
 micromamba repoquery whoneeds libpysal -c conda-forge > whoneeds.txt
 
