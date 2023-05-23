9e6938a0222033a9a1148687513df0473023e4a9
Martin Fleischmann <martin@martinfleischmann.net>
2023-05-23 12:13:19 -0700

    use input

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,8 +13,8 @@ micromamba install -y -n base python -c conda-forge
 eval "$(micromamba shell hook --shell=bash)"
 micromamba activate base
 
-# micromamba repoquery whoneeds $INPUT_PACKAGE_NAME -c conda-forge > whoneeds.txt
-micromamba repoquery whoneeds libpysal -c conda-forge > whoneeds.txt
+micromamba repoquery whoneeds $INPUT_PACKAGE_NAME -c conda-forge > whoneeds.txt
+# micromamba repoquery whoneeds libpysal -c conda-forge > whoneeds.txt
 
 python get_yml.py
 
