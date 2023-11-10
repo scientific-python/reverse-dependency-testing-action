aab6e86d80d691e92b7624690b2ce07375b3aae9
Martin Fleischmann <martin@martinfleischmann.net>
2023-11-10 10:09:54 +0100

    add verbose option

--- entrypoint.sh
+++ entrypoint.sh
@@ -56,7 +56,7 @@ do
     echo -e "$counter/$total: Running pytest for $package"
     echo -e "\e[95m================================================================================\n"
 
-    pytest --color yes --tb=no --disable-warnings -n auto --pyargs $package
+    pytest --color yes --tb=no --disable-warnings -n auto --pyargs $package ${INPUT_VERBOSE:+-v}
 
     # Get the exit code
     exit_code=$?
